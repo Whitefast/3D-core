052159d38f301918a321e3c585777668cdf7d4ed
Whitefast <52990036+Whitefast@users.noreply.github.com>
2024-03-17 21:14:51 +0200

    main

---
+++ Assets/Samples/XR Interaction Toolkit/2.5.3/custominput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class custominput : MonoBehaviour
+{
+    public void onMenu()
+    {
+        Debug.Log("Menu button has been pressed,yay!");
+
+    }
+}
